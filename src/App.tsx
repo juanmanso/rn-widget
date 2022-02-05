23c444455f190ad9606f87711a6d9fedcc932a8b
Juan Manso <juanmanso@gmail.com>
2022-02-05 22:35:17 +0100

    feat(react-native): add shared items for iOS and Android widgets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, NativeModules, SafeAreaView, Text} from 'react-native';
+
+import SharedGroupPreferences from 'react-native-shared-group-preferences';
+
+const group = 'group.widgetTestAsap';
+
+const SharedStorage = NativeModules.SharedStorage;
+const TITLE = 'Hello from React Native';
 
 const App = () => {
+  const [text, setText] = useState(TITLE);
+  const widgetData = {
+    text,
+  };
+
+  const handleUpdateText = async () => {
+    setText(TITLE);
+
+    try {
+      // iOS
+      await SharedGroupPreferences.setItem('widgetKey', widgetData, group);
+    } catch (error) {
+      console.log({error});
+    }
+    // Android
+    SharedStorage.set(JSON.stringify({text}));
+  };
+
   return (
-    <View>
-      <Text>Hello from React Native</Text>
-    </View>
+    <SafeAreaView>
+      <Text>{text}</Text>
+      <Button title="Update widget text" onPress={handleUpdateText} />
+    </SafeAreaView>
   );
 };
 
