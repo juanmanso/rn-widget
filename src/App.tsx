3707bbed0db9e858876d3a70eefd406de81b197e
Juan Manso <juanmanso@gmail.com>
2022-02-05 23:40:09 +0100

    feat(react-native): add input capabilities to change the text

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,65 @@
 import React, {useState} from 'react';
-import {Button, NativeModules, SafeAreaView, Text} from 'react-native';
+import {
+  Button,
+  NativeModules,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
 
 import SharedGroupPreferences from 'react-native-shared-group-preferences';
 
 const group = 'group.widgetTestAsap';
 
 const SharedStorage = NativeModules.SharedStorage;
-const TITLE = 'Hello from React Native';
 
 const App = () => {
-  const [text, setText] = useState(TITLE);
+  const [text, setText] = useState('');
   const widgetData = {
     text,
   };
 
   const handleUpdateText = async () => {
-    setText(TITLE);
-
     try {
-      // iOS
-      await SharedGroupPreferences.setItem('widgetKey', widgetData, group);
+      if (Platform.OS === 'android') {
+        SharedStorage.set(JSON.stringify({text}));
+      } else {
+        await SharedGroupPreferences.setItem('widgetKey', widgetData, group);
+      }
     } catch (error) {
       console.log({error});
     }
-    // Android
-    SharedStorage.set(JSON.stringify({text}));
   };
 
   return (
     <SafeAreaView>
-      <Text>{text}</Text>
-      <Button title="Update widget text" onPress={handleUpdateText} />
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={newText => setText(newText)}
+          value={text}
+          returnKeyType="send"
+          placeholder="Enter the text to display..."
+        />
+        <Button title="Update widget text" onPress={handleUpdateText} />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    borderBottomWidth: 1,
+    fontSize: 20,
+    minHeight: 40,
+  },
+});
 
 export default App;
